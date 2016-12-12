4bc5582e9d850dd3e961185edcf25f82c5de4136
Leon <xiaoyankeji@hotmail.com>
2016-12-12 21:29:49 +0800

    封装为用户控件

--- 支持透明和滚动条组合文本框控件/MainForm.Designer.vb
+++ 支持透明和滚动条组合文本框控件/MainForm.Designer.vb
@@ -22,63 +22,27 @@ Partial Class MainForm
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.LabelPanel = New System.Windows.Forms.Label()
-        Me.LabelScrollBar = New System.Windows.Forms.Label()
-        Me.LabelTextBox = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.LeonTextBox1 = New 支持透明和滚动条组合文本框控件.LeonTextBox()
         Me.SuspendLayout()
-        '
-        'LabelPanel
-        '
-        Me.LabelPanel.BackColor = System.Drawing.Color.Transparent
-        Me.LabelPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.LabelPanel.Location = New System.Drawing.Point(349, 9)
-        Me.LabelPanel.Name = "LabelPanel"
-        Me.LabelPanel.Size = New System.Drawing.Size(275, 360)
-        Me.LabelPanel.TabIndex = 1
-        '
-        'LabelScrollBar
-        '
-        Me.LabelScrollBar.BackColor = System.Drawing.Color.Transparent
-        Me.LabelScrollBar.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.LabelScrollBar.ForeColor = System.Drawing.Color.White
-        Me.LabelScrollBar.Image = Global.支持透明和滚动条组合文本框控件.My.Resources.UnityResource.ScrollBar
-        Me.LabelScrollBar.Location = New System.Drawing.Point(565, 51)
-        Me.LabelScrollBar.Name = "LabelScrollBar"
-        Me.LabelScrollBar.Size = New System.Drawing.Size(32, 72)
-        Me.LabelScrollBar.TabIndex = 3
-        Me.LabelScrollBar.Visible = False
-        '
-        'LabelTextBox
-        '
-        Me.LabelTextBox.AutoSize = True
-        Me.LabelTextBox.BackColor = System.Drawing.Color.Transparent
-        Me.LabelTextBox.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.LabelTextBox.ForeColor = System.Drawing.Color.White
-        Me.LabelTextBox.Location = New System.Drawing.Point(318, 179)
-        Me.LabelTextBox.Name = "LabelTextBox"
-        Me.LabelTextBox.Size = New System.Drawing.Size(0, 20)
-        Me.LabelTextBox.TabIndex = 4
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(13, 9)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox1.Size = New System.Drawing.Size(330, 329)
-        Me.TextBox1.TabIndex = 5
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(13, 345)
+        Me.Button1.Location = New System.Drawing.Point(521, 12)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(330, 24)
+        Me.Button1.Size = New System.Drawing.Size(103, 354)
         Me.Button1.TabIndex = 6
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LeonTextBox1
+        '
+        Me.LeonTextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.LeonTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LeonTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.LeonTextBox1.Name = "LeonTextBox1"
+        Me.LeonTextBox1.Size = New System.Drawing.Size(503, 354)
+        Me.LeonTextBox1.TabIndex = 7
         '
         'MainForm
         '
@@ -88,21 +52,13 @@ Partial Class MainForm
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(636, 378)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.LabelTextBox)
-        Me.Controls.Add(Me.LabelScrollBar)
-        Me.Controls.Add(Me.LabelPanel)
+        Me.Controls.Add(Me.LeonTextBox1)
         Me.DoubleBuffered = True
         Me.Name = "MainForm"
         Me.Text = "支持透明和滚动条组合文本框控件"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents LabelPanel As Label
-    Friend WithEvents LabelScrollBar As Label
-    Friend WithEvents LabelTextBox As Label
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents LeonTextBox1 As LeonTextBox
 End Class
--- 支持透明和滚动条组合文本框控件/MainForm.vb
+++ 支持透明和滚动条组合文本框控件/MainForm.vb
@@ -1,68 +1,14 @@
 ﻿Public Class MainForm
-    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        LabelPanel.BackColor = Color.FromArgb(50, Color.White)
-        LabelTextBox.Parent = LabelPanel
-        LabelScrollBar.Parent = LabelPanel
-        LabelTextBox.Location = Point.Empty
-        LabelScrollBar.Location = New Point(LabelPanel.Width - LabelScrollBar.Width, 0)
-
-        For index As Integer = 0 To 30
-            LabelTextBox.Text &= index & vbCrLf
-        Next
-    End Sub
-
-    Private Sub LabelScrollBar_MouseDown(sender As Object, e As MouseEventArgs) Handles LabelScrollBar.MouseDown
-        LabelPanel.Tag = MousePosition.Y : LabelScrollBar.Tag = LabelScrollBar.Top
-        AddHandler LabelScrollBar.MouseMove, AddressOf LabelScrollBar_MouseMove
-    End Sub
-
-    Private Sub LabelScrollBar_MouseMove(sender As Object, e As MouseEventArgs)
-        If LabelTextBox.Height <= LabelPanel.Height Then Exit Sub
-        Dim TopPosition As Integer = MousePosition.Y - LabelPanel.Tag + LabelScrollBar.Tag
-        If TopPosition < 0 Or TopPosition > LabelPanel.Height - LabelScrollBar.Height Then Exit Sub
-        LabelScrollBar.Top = TopPosition
-        LabelTextBox.Top = -(LabelTextBox.Height - LabelPanel.Height) * (LabelScrollBar.Top / (LabelPanel.Height - LabelScrollBar.Height))
-    End Sub
-
-    Private Sub LabelScrollBar_MouseUp(sender As Object, e As MouseEventArgs) Handles LabelScrollBar.MouseUp
-        RemoveHandler LabelScrollBar.MouseMove, AddressOf LabelScrollBar_MouseMove
-    End Sub
-
-    Private Sub LabelTextBox_Resize(sender As Object, e As EventArgs) Handles LabelTextBox.Resize
-        If LabelTextBox.Height > LabelPanel.Height Then
-            If Not LabelScrollBar.Visible Then
-                LabelScrollBar.Top = 0
-                LabelScrollBar.Show()
-            End If
-        Else
-            If LabelScrollBar.Visible Then
-                LabelTextBox.Location = Point.Empty
-                LabelScrollBar.Hide()
-            End If
-        End If
-    End Sub
-
-    Private Sub LabelTextBox_MouseDown(sender As Object, e As MouseEventArgs) Handles LabelTextBox.MouseDown, LabelPanel.MouseDown
-        LabelPanel.Tag = MousePosition.Y : LabelTextBox.Tag = LabelTextBox.Top
-        AddHandler LabelTextBox.MouseMove, AddressOf LabelTextBox_MouseMove
-        AddHandler LabelPanel.MouseMove, AddressOf LabelTextBox_MouseMove
-    End Sub
-
-    Private Sub LabelTextBox_MouseMove(sender As Object, e As MouseEventArgs)
-        If LabelTextBox.Height <= LabelPanel.Height Then Exit Sub
-        Dim TopPosition As Integer = MousePosition.Y - LabelPanel.Tag
-        If TopPosition > -LabelTextBox.Tag Or LabelTextBox.Tag + TopPosition < LabelPanel.Height - LabelTextBox.Height Then Exit Sub
-        LabelTextBox.Top = LabelTextBox.Tag + TopPosition
-        LabelScrollBar.Top = -LabelTextBox.Top * (LabelPanel.Height - LabelScrollBar.Height) / (LabelTextBox.Height - LabelPanel.Height)
-    End Sub
-
-    Private Sub LabelTextBox_MouseUp(sender As Object, e As MouseEventArgs) Handles LabelTextBox.MouseUp, LabelPanel.MouseUp
-        RemoveHandler LabelTextBox.MouseMove, AddressOf LabelTextBox_MouseMove
-        RemoveHandler LabelPanel.MouseMove, AddressOf LabelTextBox_MouseMove
-    End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        LabelTextBox.Text = TextBox1.Text
+        Dim S As String, SL As Integer
+        S = Space(Rnd() * 50).Replace(" ", vbCrLf)
+        SL = S.Length
+        For Index As Integer = SL - 1 To 0 Step -1
+            S = S.Insert(Index, Space(Rnd() * 20).Replace(" ", "+"))
+        Next
+        LeonTextBox1.ScrollBar.BackColor = Color.FromArgb(150, VBMath.Rnd * 255, VBMath.Rnd * 255, VBMath.Rnd * 255)
+        LeonTextBox1.TextBox.Text = S
     End Sub
 
 End Class
